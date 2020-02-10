d53ed0c49c5470d6f730ecacf89bb467bf9561d0
functional-cron-8324 <nicholaszafiriou@gmail.com>
2020-02-10 17:13:20 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  sum
 end
 
 def subtraction(num1, num2)
-
+  res = num1 - num2
+  res
 end
 
 def division(num1, num2)
-
+  res = num1 / num2
+  res
 end
 
 def multiplication(num1, num2)
-
+  res = num1 * num2
+  res
 end
 
 def modulo(num1, num2)
-
+  res = num1 % num2
+  res
 end
 
 def square_root(num)
-
+  res = num ** 0.5
+  res
 end
